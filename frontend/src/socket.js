d39935c24978a56d417f47cdd5fe356f7060eaa7
halil <halil@SAFA>
2025-06-11 11:29:23 +0300

    socker.io config

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 
 const url = import.meta.env.VITE_SOCKETIO_API_BASE;
-const socket = io(url); // Gerekirse prod'da değiştir
+const socket = io("https://homebooking-kccp.onrender.com"); // Gerekirse prod'da değiştir
 
 export default socket; 
